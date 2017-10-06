af8673820badeb6c955db86f5785e0b199615c06
nachomieres <nachomieres@gmail.com>
2017-10-06 12:46:56 +0200

    version inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { HttpModule } from '@angular/http';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -14,7 +17,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +28,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+import { Http } from  '@angular/http';
+import 'rxjs/add/operator/map';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  datos = {};
+  constructor(public navCtrl: NavController, public barcode: BarcodeScanner, private http: Http) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  escanear () {
+    this.barcode.scan().then((barcodeData) => {
+      //console.log (barcodeData.text);
+      this.http.get('http://world.openfoodfacts.org/api/v0/product/' + barcodeData.text + '.json').map(res => res.json()).subscribe(data => {
+         console.log (data);
+         this.datos = data.product.product_name_en;
+      });
+     }, (err) => {
+         // An error occurred
+     });
   }
 
 }
